31333e367dfda3760ac26e791e1587fa5aeae538
shaheryaryousaf <shaheryaryousaf1996@gmail.com>
2024-07-28 11:41:11 +0500

    Mobile Design Fixed for Homepage

--- src/components/listings/SIngleListing.jsx
+++ src/components/listings/SIngleListing.jsx
@@ -16,7 +16,7 @@ import {
   FaThLarge,
 } from "react-icons/fa";
 
-const SIngleListing = ({ l }) => {
+const SingleListing = ({ l }) => {
   return (
     <Col lg={4} className="mb-3">
       <Card className="single_listing">
@@ -27,20 +27,20 @@ const SIngleListing = ({ l }) => {
             <FaMapMarkerAlt color="gray" /> {l.city} {l.state}, {l.zip}
           </Card.Text>
           <Row className="mt-4">
-            <Col lg={6}>
+            <Col lg={6} xs={6}>
               <FaThLarge color="gray" /> <span>SQFT: {l.square_foot}</span>
             </Col>
-            <Col lg={6}>
+            <Col lg={6} xs={6}>
               <FaCarAlt color="gray" />
               <span>Garage: {l.garage}</span>
             </Col>
           </Row>
           <Row>
-            <Col lg={6}>
+            <Col lg={6} xs={6}>
               <FaBed color="gray" />
               <span>Bedrooms: {l.bedrooms}</span>
             </Col>
-            <Col lg={6}>
+            <Col lg={6} xs={6}>
               <FaBath color="gray" />
               <span>Bathrooms: {l.bathrooms}</span>
             </Col>
@@ -52,4 +52,4 @@ const SIngleListing = ({ l }) => {
   );
 };
 
-export default SIngleListing;
+export default SingleListing;

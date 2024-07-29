5a235bbf7a2f89d5207e13c3cdaca4626afbdbd8
shaheryaryousaf <shaheryaryousaf1996@gmail.com>
2024-07-29 12:18:49 +0500

    Listings Page Done

--- src/screens/Listings.jsx
+++ src/screens/Listings.jsx
@@ -4,6 +4,15 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 
+// Import Components
+import SingleListing from "../components/listings/SingleListing";
+
+// Import Libraries
+import { Link } from "react-router-dom";
+
+// Import Dummy Data
+import { listingsData } from "../dummyData.js";
+
 // Import Icons
 import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
@@ -20,6 +29,28 @@ const Listings = () => {
                 pariatur!
               </p>
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Container className="py-4">
+        {/* Breadcrumbs */}
+        <Row>
+          <Col lg={12}>
+            <div className="breadcrumb">
+              <Link to="/">Home</Link> &nbsp; &gt; Featured Listings
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Listings Section */}
+      <section className="listings_section">
+        <Container className="py-3">
+          <Row>
+            {listingsData.map((l, index) => (
+              <SingleListing key={index} l={l} />
+            ))}
           </Row>
         </Container>
       </section>

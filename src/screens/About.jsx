a3253d0d1b316328e9f43825b7dd06fef3fd3fa9
shaheryaryousaf <shaheryaryousaf1996@gmail.com>
2024-07-30 20:12:01 +0500

    About and Listing Details Pages are done

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -3,12 +3,13 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-
-// Import Icons
-import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+import Card from "react-bootstrap/Card";
 
 // Import Libraries
 import { Link } from "react-router-dom";
+
+// Import Icons
+import { FaEnvelope, FaPhone } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -37,6 +38,106 @@ const About = () => {
             </div>
           </Col>
         </Row>
+
+        {/* About Content */}
+        <section className="content mt-4">
+          <Row>
+            <Col lg={9}>
+              <h3>We Search For The Perfect Home</h3>
+              <p className="subHeading">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
+                pariatur!
+              </p>
+              <Image src="/images/about.jpg" fluid />
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perspiciatis esse officia repudiandae ad saepe ex, amet neque
+                quod accusamus rem quia magnam magni dolorum facilis ullam
+                minima perferendis? Exercitationem at quaerat commodi id libero
+                eveniet harum perferendis laborum molestias quia.
+              </p>
+            </Col>
+            <Col lg={3}>
+              <Card>
+                <Card.Img variant="top" src="/images/realtors/jenny.jpg" />
+                <Card.Body>
+                  <Card.Title>Seller of the Month</Card.Title>
+                  <p className="text-secondary">Jenny Johnson</p>
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Omnis nesciunt amet, illo itaque similique repellat.
+                    content.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </section>
+
+        {/* CTA Section */}
+        <section className="cta_section mt-4">
+          <Row>
+            <Col lg={12} className="text-center">
+              <h1>We Work For You</h1>
+              <p className="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+                velit aperiam, unde aliquid at similique!
+              </p>
+              <Link to="/listings" className="btn btn-md btn-primary">
+                View our Featured Listings
+              </Link>
+            </Col>
+          </Row>
+        </section>
+
+        {/* Realtors List */}
+        <section className="realtors_list">
+          <Row>
+            <h3 className="text-center">Our Realtors</h3>
+
+            {/* Single Realtor */}
+            <Col lg={4} className="text-center">
+              <Image src="/images/realtors/jenny.jpg" rounded fluid />
+              <h5>Jenny Johnson</h5>
+              <div className="info">
+                <FaPhone />
+                <span>(333)-333-3333</span>
+              </div>
+              <p className="info">
+                <FaEnvelope />
+                <span>jenny@btremern.co</span>
+              </p>
+            </Col>
+
+            {/* Single Realtor */}
+            <Col lg={4} className="text-center">
+              <Image src="/images/realtors/mark.jpg" rounded fluid />
+              <h5>Mark Hudson</h5>
+              <div className="info">
+                <FaPhone />
+                <span>(444)-444-4444</span>
+              </div>
+              <p className="info">
+                <FaEnvelope />
+                <span>mark@btremern.co</span>
+              </p>
+            </Col>
+
+            {/* Single Realtor */}
+            <Col lg={4} className="text-center">
+              <Image src="/images/realtors/kyle.jpg" rounded fluid />
+              <h5>Kyle Brown</h5>
+              <div className="info">
+                <FaPhone />
+                <span>(555)-555-5555</span>
+              </div>
+              <p className="info">
+                <FaEnvelope />
+                <span>kyle@btremern.co</span>
+              </p>
+            </Col>
+          </Row>
+        </section>
       </Container>
     </>
   );
--- src/screens/ListingDetail.jsx
+++ src/screens/ListingDetail.jsx
@@ -3,6 +3,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+
+// Import Libraries
+import { Link } from "react-router-dom";
 
 // Import Icons
 import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
@@ -16,6 +22,94 @@ const ListingDetail = () => {
             <Col lg={12} className="text-center">
               <h1>45 Drivewood Circle</h1>
               <p className="mb-0">Norwood MA, 02062</p>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <Container className="py-4">
+        {/* Breadcrumbs */}
+        <Row>
+          <Col lg={12}>
+            <div className="breadcrumb">
+              <Link to="/">Home</Link> &nbsp; &gt; Featured Listings
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Listing Detail */}
+      <section className="listing_detail">
+        <Container>
+          <Row>
+            <Col lg={9}>
+              <div className="thumbnail">
+                <Image src="/images/home-1.jpg" fluid />
+              </div>
+              <div className="specs">
+                <Row>
+                  <Col lg={6}>
+                    <ListGroup>
+                      <ListGroup.Item>
+                        <span>Asking Price: </span>
+                        <span>$490,000</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Bedrooms: </span>
+                        <span>4</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Bathrooms: </span>
+                        <span>5</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Garage: </span>
+                        <span>2</span>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Col>
+                  <Col lg={6}>
+                    <ListGroup>
+                      <ListGroup.Item>
+                        <span>Square Feet: </span>
+                        <span>3500</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Lot Size: </span>
+                        <span>3 Acres</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Listing Date: </span>
+                        <span>20/07/2024</span>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <span>Realtor: </span>
+                        <span>Jenny Johnson</span>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Col>
+                </Row>
+              </div>
+
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia recusandae esse reiciendis officia omnis non rerum
+                dicta cupiditate nostrum molestias deserunt aut minus inventore
+                animi atque, consequuntur ad fugit. Possimus culpa blanditiis
+                repellendus ipsa similique ullam, natus error dolor harum.
+              </p>
+            </Col>
+            <Col lg={3}>
+              <Card>
+                <Card.Img variant="top" src="/images/realtors/jenny.jpg" />
+                <Card.Body>
+                  <Card.Title>Property Realtor</Card.Title>
+                  <p className="text-secondary">Jenny Johnson</p>
+                  <Button className="btn btn-md btn-primary w-100">
+                    Contact Realtor
+                  </Button>
+                </Card.Body>
+              </Card>
             </Col>
           </Row>
         </Container>
